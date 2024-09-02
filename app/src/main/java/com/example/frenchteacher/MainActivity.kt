e8ffe3e12654604ada9d45620143fec071651f43
Kolchin13D <kolchin13d@gmail.com>
2024-09-02 12:09:37 +0300

    testing

--- app/src/main/java/com/example/frenchteacher/MainActivity.kt
+++ app/src/main/java/com/example/frenchteacher/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.frenchteacher
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    fun SayColor(view: View) {
+        val clickedButton: Button = view as Button
+
+        var mediaPlayer: MediaPlayer = MediaPlayer.create(
+            this,
+            resources.getIdentifier(
+                clickedButton.tag.toString(),
+                "raw",
+                packageName
+            )
+        )
+
+        mediaPlayer.start()
     }
 }
